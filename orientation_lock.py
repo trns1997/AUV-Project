a56a931dcd8100c451e4fe0493904aa6cac9615b
trns1997 <trns@ust.hk>
2018-02-26 21:18:33 -0500

    orientation test

--- orientation_lock.py
+++ orientation_lock.py
@@ -3,11 +3,23 @@ import numpy as np
 import pigpio
 import time
 
-from optimizing import motor_init
 from rtimu import imu_init
 
-motor_init()
-imu_init()
+def motor_init():
+	global pi, SERVO_1, SERVO_2, init
+	SERVO_1 = 26
+	SERVO_2 = 16
+	SERVO_3 = 4
+	SERVO_4 = 12
+
+	pi = pigpio.pi()
+	init = 1460
+
+	print("init")
+	pi.set_servo_pulsewidth(SERVO_1, init)
+	pi.set_servo_pulsewidth(SERVO_2, init)
+	pi.set_servo_pulsewidth(SERVO_3, init)
+	pi.set_servo_pulsewidth(SERVO_4, init)
 
 def orientation_correction(yaw):
 	if yaw < -5:
@@ -22,6 +34,9 @@ def orientation_correction(yaw):
 		pi.set_servo_pulsewidth(SERVO_1, speed_left)
 		pi.set_servo_pulsewidth(SERVO_2, speed_right)
 		print(speed_left, speed_right)
+
+motor_init()
+imu_init()
 
 while True:
 	if imu.IMURead():
