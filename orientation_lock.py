96f31ab88e7b1622e346e859ed8ce52414f29712
trns1997 <trns@ust.hk>
2018-02-26 21:28:59 -0500

    orientation test

--- orientation_lock.py
+++ orientation_lock.py
@@ -23,42 +23,6 @@ def motor_init():
 	pi.set_servo_pulsewidth(SERVO_3, init)
 	pi.set_servo_pulsewidth(SERVO_4, init)
 
-def imu_init():
-	global offset_roll, offset_pitch, offset_yaw, imu, cnt
-	SETTINGS_FILE = "/home/pi/Desktop/RTIMULib2/Linux/build/RTIMULibDemo/RTIMULib.ini"
-	s = RTIMU.Settings(SETTINGS_FILE)
-	imu = RTIMU.RTIMU(s)
-	if (not imu.IMUInit()):
-	  print("IMU init failed")
-	  exit(1)
-	else:
-	  print("IMU init succeeded")
-	imu.setSlerpPower(0.02)
-	imu.setGyroEnable(True)
-	imu.setAccelEnable(True)
-	imu.setCompassEnable(True)
-	poll_interval = imu.IMUGetPollInterval()
-	cnt = 0
-	offset_cnt = 0
-	offset_roll = 0
-	offset_pitch = 0
-	offset_yaw = 0
-
-	#Get offset values for roll pitch and yaw when program begins
-	while offset_cnt <=10:
-		if imu.IMURead():
-			offset_data = imu.getFusionData()
-			offset_roll += (offset_data[0])
-	      		offset_pitch += (offset_data[1])
-		       	offset_yaw += (offset_data[2])
-			offset_cnt +=1
-			sleep(0.2)
-
-	offset_roll = degrees(offset_roll/offset_cnt)
-	offset_pitch = degrees(offset_pitch/offset_cnt)
-	offset_yaw = degrees(offset_yaw/offset_cnt)
-	print("offset calculated")
-
 def orientation_correction(yaw):
 	if yaw < -5:
 		speed_left = 1460 + abs(yaw)
@@ -74,7 +38,40 @@ def orientation_correction(yaw):
 		print(speed_left, speed_right)
 
 motor_init()
-imu_init()
+
+SETTINGS_FILE = "/home/pi/Desktop/RTIMULib2/Linux/build/RTIMULibDemo/RTIMULib.ini"
+s = RTIMU.Settings(SETTINGS_FILE)
+imu = RTIMU.RTIMU(s)
+if (not imu.IMUInit()):
+  print("IMU init failed")
+  exit(1)
+else:
+  print("IMU init succeeded")
+imu.setSlerpPower(0.02)
+imu.setGyroEnable(True)
+imu.setAccelEnable(True)
+imu.setCompassEnable(True)
+poll_interval = imu.IMUGetPollInterval()
+cnt = 0
+offset_cnt = 0
+offset_roll = 0
+offset_pitch = 0
+offset_yaw = 0
+
+#Get offset values for roll pitch and yaw when program begins
+while offset_cnt <=10:
+	if imu.IMURead():
+		offset_data = imu.getFusionData()
+		offset_roll += (offset_data[0])
+      		offset_pitch += (offset_data[1])
+	       	offset_yaw += (offset_data[2])
+		offset_cnt +=1
+		sleep(0.2)
+
+offset_roll = degrees(offset_roll/offset_cnt)
+offset_pitch = degrees(offset_pitch/offset_cnt)
+offset_yaw = degrees(offset_yaw/offset_cnt)
+print("offset calculated")
 
 while True:
 	if imu.IMURead():
